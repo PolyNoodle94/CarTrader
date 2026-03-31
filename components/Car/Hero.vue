2f0d6fc1d88db8d7727d7a863526352c0db0e1f9
Emilio <emilio@parone.com>
2026-03-31 11:02:51 -0500

    Created a few components and created a /city/[city]/car/[[make]] route

---
+++ components/Car/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- HOME HERO Start -->
+    <!-- Background image spanning whole screen -->
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://carconfigurator.ferrari.com/rt-assets/data/cars/purosangue/ui/splashpage.jpg')]">
+        <div class="z-10 absolute w-full h-full flex justify-center items-center ">
+        <div class="text-center w-1/2">
+            <h1 class="text-8xl font-serif mb-10 text-white">Find your Drive</h1>
+            <!-- HOME SEARCH BAR Start -->
+            <CarSearchBar />    
+            <!-- HOME SEARCH BAR End   -->
+        </div>
+        </div>
+        <!-- Give a dark tint -->
+        <div class="z-1 absolute w-full h-full opacity-50 bg-black"></div>
+    </div>
+    <!-- HOME HERO End   -->
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- NAVBAR Start -->
+    <!-- Using TailwindCSS: If unsure of any setting "sticky", "justify-between", etc... go to https://tailwindcss.com/ and insert the setting in the Search bar -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font0mono" href="">CarTrader</a>
+    </header>
+    <!-- NAVBAR End   -->
+</template>
