57f099492cf1a1783535ef9996ada020bca75573
Emilio <emilio@parone.com>
2026-05-17 15:28:55 -0500

    implemented some SideBar functionality for changing locations and activating a modal. Also added a comment to the error thrown in [name]-[id].vue

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -16,6 +16,9 @@ useHead({title: toTitleCase(route.params.name)})
 definePageMeta({layout: "custom"})
 
 if (!car.value) {
+    // Server-side error if user attempts to edit URL to travel to a page that doesnt exist
+    // Its server side since Nuxt SSR's the initial load the page, refreshing the page / editing the URL directly would cause this page to be SSR'd,
+    // therefore this error being a server side error
     throw createError({
         statusCode: 404,
         message: `Car with ID of ${route.params.id} does not exist`
